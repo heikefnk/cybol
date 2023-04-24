b939a980d7349c2003d19d8b383fb183e14ee3c4
Victor Bello <you@example.com>
2023-04-24 18:04:36 +0100

    f

--- pages/api/hello.ts
+++ pages/api/hello.ts
@@ -1,9 +1,7 @@
 // Next.js API route support: https://nextjs.org/docs/api-routes/introduction
-import dotenv from "dotenv"
 import type { NextApiRequest, NextApiResponse } from 'next'
 import nodemailer from 'nodemailer'
 
-dotenv.config()
 
 type Data = {
   message: string
